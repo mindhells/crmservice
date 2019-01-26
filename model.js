35a33e8fa81ca1a6faf08c71141359db376bbf2a
Alberto Gonzalez <mindhells@gmail.com>
2019-01-26 16:06:58 +0000

    add users CRUD with password encrypt

--- model.js
+++ model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 mongoose.connect(
     process.env.MONGODB_URI,
@@ -18,6 +19,31 @@ const CustomerSchema = new mongoose.Schema({
 
 CustomerSchema.set('toObject', { virtuals: true })
 
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hash: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+})
+
+UserSchema.set('toObject', { virtuals: true })
+
+// password encrypt/verify
+UserSchema.virtual('password').set(function(value) {
+    const salt = bcrypt.genSaltSync(10)
+    this.hash = bcrypt.hashSync(value, salt)
+})
+
 module.exports = {
-    Customer: mongoose.model('Customer', CustomerSchema)
+    Customer: mongoose.model('Customer', CustomerSchema),
+    User: mongoose.model('User', UserSchema)
 }
--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,4 @@
-const { Customer } = require('./model')
+const { Customer, User } = require('./model')
 
 module.exports = {
     Query: {
@@ -9,6 +9,14 @@ module.exports = {
         async customers() {
             const customers = await Customer.find()
             return customers.map(customer => customer.toObject())
+        },
+        async user(_, { id }) {
+            const user = await User.findById(id)
+            return user ? user.toObject() : null
+        },
+        async users() {
+            const users = await User.find()
+            return users.map(user => user.toObject())
         }
     },
     Mutation: {
@@ -17,12 +25,28 @@ module.exports = {
             return customer.toObject()
         },
         async editCustomer(_, { id, input }) {
-            const customer = await Customer.findByIdAndUpdate(id, input)
+            const customer = await Customer.findByIdAndUpdate(id, input,  { new: true })
             return customer ? customer.toObject() : null 
         },
         async deleteCustomer(_, { id }) {
             const customer = await Customer.findByIdAndDelete(id)
             return customer ? customer.toObject() : null 
+        },
+        async addUser(_, { input }) {
+            const user = await User.create(input) 
+            return user.toObject()
+        },
+        async editUser(_, { id, input }) {
+            const user = await User.findById(id)
+            if (user) {
+                user.set(input)
+                await user.save()
+            }
+            return user
+        },
+        async deleteUser(_, { id }) {
+            const user = await User.findByIdAndDelete(id)
+            return user ? user.toObject() : null
         }
     }
 }
